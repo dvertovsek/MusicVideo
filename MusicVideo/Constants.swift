c1a384dbeb72ead54b768aad60391594b47acffe
Darijan <dvertovs@foi.hr>
2016-06-13 09:59:31 +0200

    part 4 completed

---
+++ MusicVideo/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  MusicVideo
+//
+//  Created by FOI on 13/06/16.
+//  Copyright © 2016 Darijan Vertovsek. All rights reserved.
+//
+
+import Foundation
+
+typealias JSONDictionary = [String:AnyObject]
+
+typealias JSONArray = Array<AnyObject>
